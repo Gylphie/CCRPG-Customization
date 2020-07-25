441516bc76a2ce4875e8399efe02f80b88833c01
Gylphie <seankeoughbeast@gmail.com>
2020-07-25 14:33:45 -0400

    Initial commit

---
+++ Scripts/Breasts.js
@@ -0,0 +1,29 @@
+<<nobr>>
+
+<<set $boobN to random(1,7)>>
+
+/* boob */
+
+<<if $boobN == 1>>
+    <<set $boob = "None/Flat">>
+<<elseif $boobN == 2>>
+    <<set $boob = "Tiny">>
+<<elseif $boobN == 3>>
+    <<set $boob = "Small">>
+<<elseif $boobN == 4>>
+    <<set $boob = "Average">>
+<<elseif $boobN == 5>>
+    <<set $boob = "Large">>
+<<elseif $boobN == 6>>
+    <<set $boob = "Huge">>
+<<elseif $boobN == 7>>
+    <<set $boob = "Enormous">>
+<</if>>
+
+Breast Size: $boob
+
+<</nobr>>
+
+Would you like penis and balls?
+[[Yes|Penis]]
+[[No|Vagina Question]]
